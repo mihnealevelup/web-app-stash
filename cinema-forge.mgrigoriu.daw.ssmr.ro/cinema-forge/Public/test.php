9c28377e7685da483f9e214f0017a59d5f8a6c4f
mihnea.grigoriu <mihnea-andrei.grigoriu@s.unibuc.ro>
2026-09-02 23:49:11 +0300

    Remove the duplicated layout stubs under Views and the leftover debug logging

---
+++ cinema-forge.mgrigoriu.daw.ssmr.ro/cinema-forge/Public/test.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h1>Cinema-Forge Test</h1>";
+echo "<p>Document Root: " . $_SERVER['DOCUMENT_ROOT'] . "</p>";
+echo "<p>Script Path: " . __FILE__ . "</p>";
+echo "<p>Server Name: " . $_SERVER['SERVER_NAME'] . "</p>";
+?>
